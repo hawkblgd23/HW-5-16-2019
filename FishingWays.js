4d2d309d39fa06902ea2c47cd0ad50c33a586ae9
hawkblgd23 <50380120+hawkblgd23@users.noreply.github.com>
2019-05-16 15:06:41 -0500

    Add files via upload

---
+++ FishingWays.js
@@ -0,0 +1,19 @@
+var Mississippi = 'Crappie';
+var NewOrleans = 'Shrimp'
+if (Mississippi === 'Crappie') {
+    console.log('The Feast ' + Mississippi);
+} else if (NewOrleans === 'Shrimp') {
+    console.log('Best City by Far ' + NewOrleans);
+} else {
+    console.log('Dont mess with the Big Easy');
+}
+
+function Mardigras()
+{console.log ('The Big Easy');}
+Mardigras();
+
+function Katrina(name) {
+var fullname = name + ' Katrina';
+console.log (fullname);
+}
+Katrina('Hurricane');
